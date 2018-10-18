0a92fc0f9b45eaf6f153aea72cc136976526e90c
zzzgit <zzzgoo@gmail.com>
2018-10-18 09:39:52 +0800

    delete files for gql api

--- bin/gql.will.be.removed/upload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-data=`node ./bin/generator.js upload`
-echo $data
-curl -H "Authorization: bearer 620538d81b54df8bf36ebe51ad9751126a7106c8" -X POST -d "$data" https://api.github.com/graphql
